4b189b74744fa29483bfaeb517c3dc7a37a4c7eb
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-11 12:40:42 +0530

    Put and Delete api done

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -99,6 +99,12 @@ const deleteBook = async function (req, res) {
 }
 
 
+// -------------validators-----------
+const regType01 = /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/ //ISBN
+const regType02 = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/ //Date
+// const regType03 = /^[A-Za-z0-9]{24}$/
+const regType04 = /^[A-Za-z, ]{4,}$/ //
+
 //------------creation---------------------
 const createBook = async function (req, res) {
     try {
@@ -124,29 +130,29 @@ const createBook = async function (req, res) {
         if (!isValid(category)) {
             return res.status(400).send({ status: false, message: "Category is mandatory and should be 'String Type'" });
         }
-        if (!isValid(subcategory) || !regType04.test(subcategory)) {
+        if (!isValid(subcategory)|| !regType04.test(subcategory)) {
             return res.status(400).send({ status: false, message: "Subcategory is mandatory and should be 'String inside an Array'" });
         }
         if (!isValid(releasedAt) || !regType02.test(releasedAt)) {
             return res.status(400).send({ status: false, message: "Enter the date in YYYY-MM-DD format, is mandatory field." });
         }
         // -------Data Base Search------------
-        let isbnExist = await BookModel.findOne({ ISBN: ISBN })
-        if (isbnExist) {
-            return res.status(400).send({ status: false, message: "Doublicate ISBN, not Allowed! " })
+        let isbnExist = await BookModel.findOne({ISBN: ISBN})
+        if (isbnExist){
+            return res.status(400).send({status:false, message: "Doublicate ISBN, not Allowed! "})
         }
-        let userExist = await UserModel.findOne({ _id: userId })
-        if (!userExist) {
-            return res.status(400).send({ status: false, message: "user does not exist with this user ID" })
+        let userExist = await UserModel.findOne({_id:userId})
+        if(!userExist){
+            return res.status(400).send({status:false, message:"user does not exist with this user ID"})
         }
-        let titleExist = await BookModel.findOne({ title: title })
-        if (titleExist) {
-            return res.status(400).send({ status: false, message: "The title name already exists." })
+        let titleExist= await BookModel.findOne({title: title})
+        if(titleExist){
+            return res.status(400).send({status: false, message:"The title name already exists."})
         }
 
         // -----------------data creation-----------
         const createdBook = await BookModel.create(requestBody)
-        res.status(201).send({ status: true, message: "Success", data: createdBook })
+        res.status(201).send({status: true, message:"Success", data: createdBook})  
 
     }
     catch (err) {
@@ -155,6 +161,5 @@ const createBook = async function (req, res) {
 
 }
 
-module.exports = { createBook, updateBook, deleteBook, }
-
+module.exports = { createBook, updateBook, deleteBook,  }
 
